53b7cf5267a8d99555f8ff909ad63132fccff3f0
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2017-10-17 12:02:25 +0300

    min body width is set

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -19,10 +19,7 @@ class GuideTour extends React.Component {
   constructor() {
     super();
     this.state = {
-      isPopupShown: false,
-      popup: null,
-      // left: 0,
-      // top: 0
+      isPopupShown: false
     }
   }
 
@@ -34,7 +31,8 @@ class GuideTour extends React.Component {
       case 'showConfigPopup':
         this.setState({
           isPopupShown: true,
-          width: '500pt',
+          width: 500,
+          units: 'pt',
           title: 'Guided Tour Configuration',
           content: TourList,
           dataProps: {
@@ -45,7 +43,8 @@ class GuideTour extends React.Component {
       case 'showSettingsPopup':
         this.setState({
           isPopupShown: true,
-          width: '350pt',
+          width: 350,
+          units: 'pt',
           title: 'Tour Settings',
           content: TourSettings,
           dataProps: {
@@ -58,14 +57,18 @@ class GuideTour extends React.Component {
     }
   };
 
-  // actionClick = event => {
-  //   this.setState({
-  //     isPopupShown: !this.state.isPopupShown,
-  //     popup: ConfigPopup,
-  //     left: Math.floor(Math.random() * 200), //evt.pageX,
-  //     top: Math.floor(Math.random() * 400)   //evt.pageY
-  //   });
-  // };
+  /**
+   * Set min-width of the document to avoid sliding up a centered popup form small width.
+   * @param minWidth {number} - minimal body width (0 to restore default)
+   * @param units (string) - name of units (e.g. pt, px).
+   */
+  bodyWidthHandler = (minWidth = 0, units = 'pt') => {
+    if (minWidth) {
+      document.body.style.minWidth = minWidth + units;
+    } else {
+      document.body.style.minWidth = '';
+    }
+  };
 
   closeHandler = (event) => {
     this.setState({
@@ -75,6 +78,11 @@ class GuideTour extends React.Component {
 
   render() {
     let state = this.state;
+    if (state.isPopupShown) {
+      this.bodyWidthHandler(state.width + 10, state.units);
+    } else {
+      this.bodyWidthHandler();
+    }
     return (
       <div className="gt-container">
         <div id="popups-store">
@@ -82,7 +90,7 @@ class GuideTour extends React.Component {
             ? <Popup title={state.title}
                                        content={state.content}
                                        dataProps={state.dataProps}
-                                       width={state.width}
+                                       width={state.width + state.units}
                                        closeHandler={this.closeHandler}
             /> : null}
         </div>
