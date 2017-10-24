3780497b0147e8c3e1e9449b4bd96dee8156e4b4
AliaksandrVarachai <vorochai@gmail.com>
2017-10-24 04:35:27 +0200

    image loader moved to separate module

--- src/helpers/src-url.js
+++ src/helpers/src-url.js
@@ -1,6 +1,12 @@
+import path from 'path';
+import sourceMap from '../source-map.js';
+
 /**
  * Returns a current working directory of the running script with trailing '/'.
  * @returns {string} - current working directory or empty string if there is no src attribute.
+ * @example
+ * // returns http://localhost:9090/
+ * getCWD()
  */
 function getCWD() {
   let scripts = document.getElementsByTagName('script');
@@ -10,4 +16,19 @@ function getCWD() {
   return url.match(/^.*\//)[0];
 }
 
-export default getCWD;
+/**
+ * Returns a URL for external loaded resource (image, font etc.).
+ * @param relativePath {string} - relative path to file (must be the same as for import).
+ * @returns {string} - url on base of getCWD.
+ * @example
+ * // returns http://localhost:9090/outer-src/logo.png
+ * getURL('../images/logo.png')
+ */
+function getURL(relativePath) {
+  const imgPath = path.relative('../source-map.js', relativePath);
+  const imgDirname = path.dirname(imgPath);
+  const imgBasename = path.basename(imgPath);
+  return getCWD() + path.join(sourceMap[imgDirname], imgBasename);
+}
+
+export default getURL;
--- src/scripts/components/TourList/TourList.js
+++ src/scripts/components/TourList/TourList.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import path from 'path';
-
-import getCWD from '../../../helpers/src-url';
+import getURL from '../../../helpers/src-url';
 import Search from '../Search/Search';
 import TourItem from '../TourItem/TourItem';
 
@@ -10,21 +8,16 @@ import './TourList.css';
 
 import '../../../images/gt-logo-179x69.png';
 
-import sourceMap from '../../../source-map.js';
-const imgPath = path.relative('../../../source-map.js', '../../../images/gt-logo-179x69.png');
-const imgDirname = path.dirname(imgPath);
-const imgBasename = path.basename(imgPath);
-const imgUrl = getCWD() + path.join(sourceMap[imgDirname], imgBasename);
-
 TourList.propTypes = {
   tourList: PropTypes.array.isRequired
 };
 
 export default function TourList({tourList}) {
+  const logoURL = getURL('../../../images/gt-logo-179x69.png');
   return (
     <div className="gt-tours-list-component">
       <div styleName="logo">
-        <img src={imgUrl} alt="logo"/>
+        <img src={logoURL} alt="logo"/>
       </div>
       <div styleName="main-header">Available Guided Tours
         <Search/> {/* TODO: add props with saved search */}
