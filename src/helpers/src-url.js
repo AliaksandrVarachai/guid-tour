88518b342510e6c04f225595782e7a23a61cb1fe
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2017-10-24 12:25:02 +0300

    image is loaded from epam server

--- src/helpers/src-url.js
+++ src/helpers/src-url.js
@@ -1,17 +1,25 @@
 import path from 'path';
 import sourceMap from '../source-map.js';
+import { GT_SCRIPT_ID } from '../constants/constants';
 
 /**
- * Returns a current working directory of the running script with trailing '/'.
+ * Returns a current working directory of the current script with trailing '/'.
  * @returns {string} - current working directory or empty string if there is no src attribute.
  * @example
  * // returns http://localhost:9090/
  * getCWD()
  */
 function getCWD() {
-  let scripts = document.getElementsByTagName('script');
-  let src = scripts[scripts.length - 1].src;
-  if (!src) return '';
+  let script = document.getElementById(GT_SCRIPT_ID);
+  if (!script) {
+    console.log(`Error: Your script must have id="${GT_SCRIPT_ID}"`);
+    return '';
+  }
+  let src = document.getElementById('guide-tour-script').src;
+  if (!src) {
+    console.log(`Error: script with id="${GT_SCRIPT_ID}" does not have src`);
+    return '';
+  }
   let url = src.match(/^[^?#]+(=?|#)/)[0];
   return url.match(/^.*\//)[0];
 }
